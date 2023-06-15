e7641c786e30388b14e5721a60fff078fea0d267
Michał Grzelka <michal.grzelka0@gmail.com>
2023-06-15 12:34:26 +0200

    Initial commit

---
+++ src/test/java/com/ethereum/manageusers/ManageusersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethereum.manageusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageusersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
